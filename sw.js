8e7b266bd001e423b05db0358152ca136df8043c
tuanhungsenpai7-web <tuanhungsenpai7@gmail.com>
2026-05-25 16:29:10 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'my-app-v1';
+// Chỉ cần khai báo chính file index của bạn
+const ASSETS = [
+  '/', 
+  '/index.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
